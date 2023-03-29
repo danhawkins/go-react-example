87eccc930a1f0baa5df1d0010641568dd1b63047
Danny Hawkins <danny@quiqup.com>
2023-03-29 09:16:08 +0400

    Split handlers out to make code more readable

--- main.go
+++ main.go
@@ -14,37 +14,45 @@ import (
 var content embed.FS
 
 func main() {
+	// Create a file server for the embedded assets
+	assetsServer := http.FileServer(http.FS(content))
 
 	// Create a new gin router
 	r := gin.Default()
 
 	api := r.Group("/api")
 	{
-		api.GET("/health", func(c *gin.Context) {
-			// Write a 200 response with JSON data
-			data := map[string]string{
-				"message": "Success",
-			}
-			c.JSON(http.StatusOK, data)
-		})
+		api.GET("/health", healthHandler)
 	}
 
-	assetsServer := http.FileServer(http.FS(content))
-	r.GET("/assets/*filepath", func(c *gin.Context) {
-		c.Request.URL.Path = "client/dist/assets/" + c.Param("filepath")
-		assetsServer.ServeHTTP(c.Writer, c.Request)
-	})
+	r.GET("/assets/*filepath", func(c *gin.Context) { staticHandler(c, assetsServer) })
 
-	r.GET("/", func(c *gin.Context) {
-		// Serve the index.html file from the embedded assets
-		data, err := content.ReadFile("client/dist/index.html")
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		c.Data(http.StatusOK, "text/html; charset=utf-8", data)
-	})
+	r.GET("/", indexHandler)
 
 	// Start the server
 	r.Run(":3000")
+}
+
+// Return the index.html from
+func indexHandler(c *gin.Context) {
+	// Serve the index.html file from the embedded assets
+	data, err := content.ReadFile("client/dist/index.html")
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	c.Data(http.StatusOK, "text/html; charset=utf-8", data)
+}
+
+// Health check handler
+func healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
+
+// Static file handler
+func staticHandler(c *gin.Context, assetServer http.Handler) {
+	c.Request.URL.Path = "client/dist/assets/" + c.Param("filepath")
+	assetServer.ServeHTTP(c.Writer, c.Request)
 }
